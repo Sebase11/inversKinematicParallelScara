5f02ce23f6c13e8cd74b3a3eee9ab28cba1cad19
Sebase11 <95432287+Sebase11@users.noreply.github.com>
2023-06-02 13:31:01 +0200

    Update inversKinematic.py

--- inversKinematic.py
+++ inversKinematic.py
@@ -2,8 +2,9 @@ import numpy as np
 import math
 
 
-x = float(input("enter x value: ")) * 10
-y = float(input("enter y value: ")) * 10
+x = float(input("enter x value in cm: ")) * 10
+y = float(input("enter y value in cm: ")) * 10
+info  = str(input("show more info ? y/n: "))
 
 d = 105
 farm = 90
@@ -13,43 +14,45 @@ sarm = 87.559
 
 #calculate c
 c = math.sqrt(x**2 + y**2)
-print("c:" + str(c))
 
 #calculate v angel
-
 V = math.atan(y/x)
-print("v: " + str(V))
 
 #calculate angel Y in radiants
-
-Y = math.acos((0 - 87.559**2 + 90**2 + c**2) / (2 * 90 * c))
-print("Y: " + str(Y))
+Y = math.acos((0 - sarm**2 + farm**2 + c**2) / (2 * farm * c))
 
 m1 = V + Y
 m1 = np.rad2deg(m1)
-print("m1:" + str(m1))
+
 
 #right side
 
 #calculate e
 e = math.sqrt((d-x)**2 + y**2)
-print("e: " + str(e))
 
 #calculate the angel psi in radians
 psi = math.atan(y / (d - x))
-print("psi: " + str(psi))
 
 #calculate the angel E in radiants
-E = math.acos((0 - 87.559**2 + 90**2 + e**2) / (2 * 90 * e))
-print("E: " + str(E))
+E = math.acos((0 - sarm**2 + farm**2 + e**2) / (2 * farm * e))
 
 m2 = math.pi - E - psi
 m2 = np.rad2deg(m2)
-print("m2: " + str(m2))
+
 
 #round the numbers
 m1r = round(m1)
 m2r = round(m2)
+
+if info == "y":
+    print("m2: " + str(m2))
+    print("E: " + str(E))
+    print("psi: " + str(psi))
+    print("e: " + str(e))
+    print("m1:" + str(m1))
+    print("Y: " + str(Y))
+    print("v: " + str(V))
+    print("c:" + str(c))
 
 print("The angel for motor 1 is: " + str(m1r))
 print("The angel for motor 2 is: " + str(m2r))
